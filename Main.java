b2fcf5812df781d49381a42cb9165661c1f1edde
omnya122 <100925046+omnya122@users.noreply.github.com>
2023-05-22 15:00:44 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,82 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/library";
+    public static void main(String[] args)
+    {
+        String url = "jdbc:mysql://localhost:3306/myschema";
         String username = "root";
-        String password = "2468mariam";
+        String password = "01120231819Omnia";
+
+
+        // 1st insert statement
+        System.out.println("This Query is to INSERT VALUES to Author_ID and Name, INTO Author TABLE");
+
+        String query1 = """
+                INSERT INTO Author (Author_ID, Name)
+                VALUES (?, ?)
+                """;
+
+        System.out.println(query1);
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             // PreparedStatement created using SQl query
+             PreparedStatement statement = connection.prepareStatement(query1))
+        {
+            // setting values to the 1st statement
+            statement.setInt(1, 13);
+            statement.setString(2, "Agatha Christie");
+
+            // Execute the 1st INSERT statement
+            int rowsAffected = statement.executeUpdate();
+            System.out.println(rowsAffected + " record(s) inserted into Author.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // 2nd insert statement
+        System.out.println("This Query is to INSERT VALUES INTO Book TABLE");
+        String query2 = """
+                INSERT INTO Book (ISBN, Publisher_Name, Title, Publication_Year, Admin_ID, Author_ID)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """;
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             // PreparedStatement created using SQl query
+             PreparedStatement statement = connection.prepareStatement(query2)) {
+            // insert ISPN
+            statement.setInt(1, 99778655);
+
+            // insert Publisher_Name
+            statement.setString(2, "Penguin Random House");
+
+            // insert Title
+            statement.setString(3, "The Enchanted Garden");
+
+            // insert Publication_Year
+            statement.setInt(4, 1976);
+
+            // insert Admin_ID
+            statement.setInt(5, 1);
+
+            // insert Author_ID
+            statement.setInt(6, 6);
+
+            // Execute the 2nd INSERT statement
+           int rowsAffected = statement.executeUpdate();
+           System.out.println(rowsAffected + " record(s) inserted into Author.");
+
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
         System.out.println("This Query is to delete the students who borrowed the book whose title 'The Great Gatsby'");
-        String query = """
+
+        // 1st Delete statement
+        String query3 = """
                 DELETE FROM student
                 WHERE Student_ID IN (
                 \tSELECT Borrower_ID
@@ -20,194 +87,178 @@ public class Main {
                 \t\tWHERE Title = 'The Great Gatsby'
                 \t)
                 );""";
-        System.out.println(query);
+        System.out.println(query3);
         try (Connection connection = DriverManager.getConnection(url, username, password);
-             PreparedStatement statement = connection.prepareStatement(query)) {
+             // PreparedStatement created using SQl query
+             PreparedStatement statement = connection.prepareStatement(query3))
+        {
             int rowsAffected = statement.executeUpdate();
             System.out.println(rowsAffected + " record(s) deleted successfully.");
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
+
+        // 2nd Delete Statement
         System.out.println("\nDelete all loan records where the corresponding books have been published after the year 2000");
-        String query2 = """
+        String query4 = """
                 DELETE FROM Loan
                 WHERE Book_ID IN (
                   SELECT ISBN
                   FROM Book
                   WHERE Publication_Year > 2000
                 );""";
-        System.out.println(query2);
+        System.out.println(query4);
         try (Connection connection = DriverManager.getConnection(url, username, password);
-             PreparedStatement statement = connection.prepareStatement(query2)) {
+             PreparedStatement statement = connection.prepareStatement(query4))
+        {
             int rowsAffected = statement.executeUpdate();
             System.out.println(rowsAffected + " record(s) deleted successfully.");
-        } catch (SQLException e) {
+
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
-        /////////////////////////////////////////////////////////////////////////////
-        /**************************** sign up a new user ***************************/
-        // by saif eldeen
-        // Sign up a new admin user
-        System.out.println("\nsign up a new student");
-        // User information
-        int ID = 1; // Replace with the ID of the user to update
-        String userType = "admin"; // Change to "student" if signing up a student
-        String name = "John Doe";
-        String email = "johndoe@example.com";
-        String sign_password = "123";
-        String phone = "01111111111";
-        SignUpUser signUpUser = new SignUpUser();
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            if (userType.equals("admin")) {
-                signUpUser.signUpAdmin(connection,ID, name, email, sign_password);
-            } else if (userType.equals("student")) {
-                signUpUser.signUpStudent(connection, ID, name, email, sign_password, phone);
-            } else {
-                System.out.println("Invalid user type");
+
+        // 1st update statement
+        System.out.println("This Query is to update borrower Phone in the Borrower table");
+
+        String query5 = """
+                UPDATE Borrower
+                SET Phone = ?
+                WHERE Borrower_ID = ?
+                     
+                """;
+        System.out.println(query5);
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query5))
+        {
+            // Borrower_ID
+            statement.setInt(1, 22344665);
+            // Phone
+            statement.setInt(2, 4);
+
+            int rowsAffected = statement.executeUpdate();
+            System.out.println(rowsAffected + " record(s) updated successfully.");
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        // 2nd update statement
+        System.out.println("This Query is to update the Publisher name, Address,and Phone");
+
+        String query6 = """
+                UPDATE Publisher
+                SET Address = ? ,
+                    Phone = ? 
+                WHERE Name = ?
+                     
+                """;
+        System.out.println(query6);
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query6)) {
+            // Name
+            statement.setString(1, "123 Street 8");
+            // Address
+            statement.setInt(2, 5554667);
+            // phone Penguin Random House
+            statement.setString(3, "Penguin Random House");
+
+            int rowsAffected = statement.executeUpdate();
+            System.out.println(rowsAffected + " record(s) updated successfully.");
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        // Select data from Author table
+        System.out.println("This Query is to retrieve data from Author table");
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement()) {
+            String query7 = """
+                    SELECT Author_ID, Name
+                    FROM Author
+                    """;
+
+            // Execute
+            ResultSet resultSet = statement.executeQuery(query7);
+
+            while (resultSet.next()) {
+                // Access the data from the result set
+                int authorId = resultSet.getInt("Author_id");
+                String authorName = resultSet.getString("Name");
+
+                // Print the retrieved data
+                System.out.println("Author ID: " + authorId);
+                System.out.println("Name: " + authorName);
+                System.out.println("----------------------");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("\n\n");
+
+        // Select data from Borrower table
+        System.out.println("This Query is to retrieve data from Borrower table");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement()) {
+            String query8 = """
+                    SELECT Borrower_ID, phone
+                    FROM Borrower
+                    """;
+            ResultSet resultSet = statement.executeQuery(query8);
+            while (resultSet.next()) {
+
+                int borrowerId = resultSet.getInt("Borrower_ID");
+                String borrowerPhone = resultSet.getString("Phone");
+
+                System.out.println("Borrower_ID: " + borrowerId);
+                System.out.println("Phone : " + borrowerPhone);
+                System.out.println("----------------------");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        /**************************** update user information ***************************/
-        // by saif eldeen
-        // User information to update
-        System.out.println("\nupdate user information");
-        int userId = 1; // Replace with the ID of the user to update
-        String userType_to_update = "admin"; // Change to "student" if updating a student
-        String newName = "saif eldeen";
-        String newEmail = "saifeldeen201770@gmail.com";
-        String newPassword = "1234";
-        String newPhone = "01222222222";
-        UpdateUser updateUser = new UpdateUser();
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            if (userType.equals("admin")) {
-                updateUser.updateAdminDetails(connection, userId, newName, newEmail, newPassword);
-            } else if (userType.equals("student")) {
-                updateUser.updateStudentDetails(connection, userId, newName, newEmail, newPassword , newPhone);
-            } else {
-                System.out.println("Invalid user type");
+        System.out.println("\n\n");
+
+        // Select data that involves more than one table of the database
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement()) {
+            String query9 = """
+                    SELECT Book.Title , Book.Publication_Year , Author.Name, Publisher.Address
+                    FROM Book JOIN Author 
+                    ON Book.Author_ID = Author.Author_ID
+                    JOIN Publisher ON Book.Publisher_Name = Publisher.Name;
+                    """;
+
+            // Execute query
+            ResultSet resultSet = statement.executeQuery(query9);
+
+            while (resultSet.next()) {
+                String bookTitle = resultSet.getString("Title");
+                String bookPublicationYear = resultSet.getString("Publication_Year");
+                String authorName = resultSet.getString("Name");
+                String publisherAddress = resultSet.getString("Address");
+
+                System.out.println("Book Title: " + bookTitle);
+                System.out.println("Author Name: " + authorName);
+                System.out.println("Publisher Address: " + publisherAddress);
+                System.out.println("Publication Year: " + bookPublicationYear);
+                System.out.println("----------------------");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        /**************************** update 2 parameters of user information ***************************/
-        // by saif eldeen
-        // User information to update
-        System.out.println("\nupdate 2 parameters of user information");
-        int userId2 = 1; // Replace with the ID of the user to update
-        String userType_to_update2 = "admin"; // Change to "student" if updating a student
-        String newName2 = "mariam";
-        String newEmail2 = "mariam@gmail.com";
-        UpdateUser updateUser2 = new UpdateUser();
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            if (userType.equals("admin")) {
-                updateUser.updateAdminDetails(connection, userId, newName, newEmail, null);
-            } else if (userType.equals("student")) {
-                // NO NEED TO UPDATE ALL THE PARAMETERS :)
-                updateUser.updateStudentDetails(connection, userId, newName, newEmail, null , null);
-            } else {
-                System.out.println("Invalid user type");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        //////////////////////////////////////////////////////////////////////////////
-        
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Select an option:");
-            System.out.println("1. Add a book");
-            System.out.println("2. Update a book");
-            int option = scanner.nextInt();
-
-            if (option == 1) {
-                addBook(connection);
-            } else if (option == 2) {
-                updateBook(connection);
-            } else {
-                System.out.println("Invalid option selected.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-    public static void addBook(Connection connection) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the book details:");
-        System.out.print("ISBN: ");
-        int ISBN = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Publisher Name: ");
-        String publisherName = scanner.nextLine();
-
-        System.out.print("Title: ");
-        String title = scanner.nextLine();
-
-        System.out.print("Publication Year: ");
-        int publicationYear = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Admin ID: ");
-        int adminID = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Author ID: ");
-        int authorID = scanner.nextInt();
-        scanner.nextLine();
-
-        Book book = new Book(ISBN, publisherName, title, publicationYear, adminID, authorID);
-        book.addBook(connection);
-    }
-
-    public static void updateBook(Connection connection) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the ISBN of the book to update: ");
-        int ISBN = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Enter the new book details:");
-        System.out.print("New Title: ");
-        String newTitle = scanner.nextLine();
-
-        System.out.print("New Publication Year: ");
-        int newPublicationYear = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("New Admin ID: ");
-        int newAdminID = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("New Author ID: ");
-        int newAuthorID = scanner.nextInt();
-        scanner.nextLine();
-
-        Book bookToUpdate = new Book(ISBN, "", "", 0, 0, 0);
-        bookToUpdate.updateBookDetails(connection, newTitle, newPublicationYear, newAdminID, newAuthorID);
+        System.out.println("\n\n");
     }
 }
-
-
-
-
-
-
-
-
-
